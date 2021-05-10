56fc7db108e0c2ffa32ac26e8fc411677188baf6
Helena Heil <helenahcodes@gmail.com>
2021-05-10 09:48:44 +0200

    remove consl

--- js/lib/create-codebuddys.js
+++ js/lib/create-codebuddys.js
@@ -1,38 +1,36 @@
-import { createElement, createDate, findCurrentWeekDay } from "./utility";
+import { createElement, createDate, findCurrentWeekDay } from './utility';
 
 export default function createBuddyPage(buddies) {
-  const buddyContainer = document.querySelector("#codebuddy__container");
+  const buddyContainer = document.querySelector('#codebuddy__container');
   buddies.forEach((team) => {
     const buddyCard = createElement(
-      "section",
-      ["buddy-card", "border__in--light"],
+      'section',
+      ['buddy-card', 'border__in--light'],
       buddyContainer
     );
 
-    console.log(buddies);
-    console.log(team);
     team.forEach((buddy, index) => {
       const firstBuddy = createElement(
-        "p",
-        ["buddy-card__item", "text__semi16--light"],
+        'p',
+        ['buddy-card__item', 'text__semi16--light'],
         buddyCard,
         buddy
       );
       if (index === 0) {
-        const divider = createElement("span", ["buddy-card__line"], buddyCard);
-        const plusSign = createElement("img", ["buddy-card__plus"], divider);
-        plusSign.src = "../images/Plus.svg";
-        plusSign.alt = "Plus Icon";
+        const divider = createElement('span', ['buddy-card__line'], buddyCard);
+        const plusSign = createElement('img', ['buddy-card__plus'], divider);
+        plusSign.src = '../images/Plus.svg';
+        plusSign.alt = 'Plus Icon';
       }
     });
   });
 }
 
 export function buddyHeader() {
-  const buddyHeader = document.querySelector("header");
+  const buddyHeader = document.querySelector('header');
   const headerDate = createElement(
-    "p",
-    ["text__regular12"],
+    'p',
+    ['text__regular12'],
     buddyHeader,
     createDate(findCurrentWeekDay())
   );
